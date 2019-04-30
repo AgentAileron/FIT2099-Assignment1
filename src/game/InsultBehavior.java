f65b4514fc06fc61567704a0a43c2459f92640ab
MolarFox <molar@molarfox.io>
2019-04-30 20:28:27 +1000

    Create insult method and action for goons

---
+++ src/game/InsultBehavior.java
@@ -0,0 +1,31 @@
+package game;
+
+import com.sun.corba.se.impl.orbutil.closure.Constant;
+
+import edu.monash.fit2099.engine.*;
+
+public class InsultBehavior extends Action implements ActionFactory{
+
+    @Override
+	public String execute(Actor actor, GameMap map) {
+        String output = actor + " hurls an insult! :\n";
+        Insults.values().
+
+        return output;
+	}
+
+    @Override
+	public Action getAction(Actor actor, GameMap map) {
+        // TODO
+    }
+
+    @Override
+	public String menuDescription(Actor actor) {
+		return "";
+	}
+
+    @Override
+	public String hotKey() {
+		return "";
+	}
+}
