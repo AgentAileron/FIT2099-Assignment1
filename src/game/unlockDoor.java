0b1877846fb3d2d129e24ff5ca734f1dcdc7acb0
sarahdennis8 <30749398+sarahddennis8@users.noreply.github.com>
2019-04-30 17:55:52 +1000

    Added key class and modified unlockDoor

--- src/game/unlockDoor.java
+++ src/game/unlockDoor.java
@@ -1,9 +1,41 @@
 package game;
+import edu.monash.fit2099.demo.Floor;
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
 
-public class unlockDoor {
+public class unlockDoor extends Action {
 
-	public unlockDoor() {
-		// TODO Auto-generated constructor stub
+	private Key usedKey;
+	private Location doorLocation;
+	private String direction;
+	
+	public unlockDoor(Key usedKey, String direction, Location doorLocation) {
+		this.usedKey = usedKey;
+		this.direction = direction;
+		this.doorLocation = doorLocation;
+	}
+	
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		if(usedKey.isSameKey(doorLocation.getGround().getKey()) {
+			return actor + " has the wrong key.";
+		}
+		else {
+			map.add(new Floor(), doorLocation);
+			return "The door has been unlocked";
+		}
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		return actor + " attempts to unlock the door to the " + direction;
+	}
+
+	@Override
+	public String hotKey() {
+		return "";
 	}
 
 }
