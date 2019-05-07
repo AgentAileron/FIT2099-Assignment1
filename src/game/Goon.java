d935bb13fee9aa4592b0841a5421df30ea2a08c4
MolarFox <molar@molarfox.io>
2019-05-07 16:35:31 +1000

    Add new classes for Goon and Ninja
    NB: Both extend Grunt

--- src/game/Goon.java
+++ src/game/Goon.java
@@ -5,12 +5,10 @@ import edu.monash.fit2099.engine.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Grunt extends Actor {
+public class Goon extends Grunt {
 
-	// Grunts have 50 hitpoints and are always represented with a g
-	public Grunt(String name, Actor player) {
-		super(name, 'g', 5, 50);
-		addBehaviour(new FollowBehaviour(player));
+	public Goon(String name, Actor player) {
+		super(name, player);
 	}
 
 	private List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
