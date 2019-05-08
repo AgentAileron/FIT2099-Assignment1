a8d32c9deae3a5cbf0b5f088ee306281215d804e
MolarFox <molar@molarfox.io>
2019-05-08 19:48:21 +1000

    Create random insult reader

--- src/game/InsultBehavior.java
+++ src/game/InsultBehavior.java
@@ -1,4 +1,8 @@
 package game;
+
+import java.io.*;
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.*;
 
 public class InsultBehavior extends Action implements ActionFactory{
@@ -23,5 +27,38 @@ public class InsultBehavior extends Action implements ActionFactory{
 	@Override
 	public String hotKey() {
 		return "";
+	}
+
+	private String randInsult(){
+		final ArrayList<String> Insults = new ArrayList<String>();
+
+		// Read in from file on first call (cache values)
+		if (Insults.size() == 0){
+			try{
+				// Buffered reader kinda overkill, but good practice / future proofing
+				FileReader reader = new FileReader("./Insults.txt");
+				BufferedReader br = new BufferedReader(reader);
+				
+				String line = br.readLine();
+
+				while (line != null) {
+					Insults.add(line);
+					line = br.readLine();
+				}
+
+				br.close();
+			} catch (IOException e) {
+				// Do nothing - empty / missing files handled below
+			}
+		}
+
+		// If still no insults (empty / missing file) return default insult
+		if (Insults.size() == 0){
+			return "Grr!";
+		}
+
+		// Pick random insult and return
+		Integer randIndex = (int)(Math.random() * Insults.size());
+		return Insults.get(randIndex);
 	}
 }
