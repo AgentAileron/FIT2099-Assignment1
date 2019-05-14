3ff2dd9678995caf69b59303c6f6d4b504ad84e7
MolarFox <molar@molarfox.io>
2019-05-14 16:43:51 +1000

    Make NPC randomwalk truly random

--- src/game/RandomWalkBehaviour.java
+++ src/game/RandomWalkBehaviour.java
@@ -3,7 +3,7 @@ package game;
 import edu.monash.fit2099.engine.*;
 
 /**
- * Behaviour handler for Q random walk - 60% chance per turn of movement happening
+ * Behaviour handler for NPC random walk - 60% chance per turn of movement happening
  * 
  * @author      Rithesh Jayaram <RJAY0006@student.monash.edu> 
  * @author      Sarah Dennis <SDEN0009@student.monash.edu>
@@ -18,13 +18,23 @@ public class RandomWalkBehaviour implements ActionFactory{
 	public Action getAction(Actor actor, GameMap map) {
         Location here = map.locationOf(actor);
         
-        if (Math.random() < 0.6){   // 60% chance that Q will move 
+        if (Math.random() < 0.6){   // 60% chance that NPC will move 
+
+            for (int i=0; i < 4; i++){
+                int exitChosen = (int) (Math.random()*3);
+                Location destination = here.getExits().get(exitChosen).getDestination();
+                if (destination.canActorEnter(actor)){
+                    return new MoveActorAction(destination, here.getExits().get(exitChosen).getName());  
+                }
+            }
+
             for (Exit exit : here.getExits()){
                 Location destination = exit.getDestination();
                 if (destination.canActorEnter(actor)){
                     return new MoveActorAction(destination, exit.getName());           
                 }
             }
+
         }
 		return new NPCSkipTurnAction();
 	}
