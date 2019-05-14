0e8ca83c7bb6d87e5f5da487d81602a2764983f0
MolarFox <molar@molarfox.io>
2019-05-14 16:49:42 +1000

    Refactor and fix adding of new moon map

--- src/game/Application.java
+++ src/game/Application.java
@@ -23,10 +23,10 @@ public class Application {
 		World world = new World(new Display());
 
 		FancyGroundFactory groundFactory = new FancyGroundFactory(new Floor(), new Wall(), new Door(), new RocketPad(), new Rocket());
-		GameMap gameMap;
-		GameMap moon;
+		GameMap lairMap;
+		GameMap moonMap;
 
-		List<String> map = Arrays.asList(
+		List<String> lairMapString = Arrays.asList(
 			".................................",
 			"...#####D#####....###............",
 			"...#.........#.....#.............",
@@ -43,10 +43,10 @@ public class Application {
 			"...........#........#.......#....",
 			"....................#########....");
 		
-		gameMap = new GameMap(groundFactory, map);
-		world.addMap(gameMap);
+		lairMap = new GameMap(groundFactory, lairMapString);
+		world.addMap(lairMap);
 		
-		List<String> moonMap = Arrays.asList(
+		List<String> moonMapString = Arrays.asList(
 				".................................",
 				".................................",
 				".................................",
@@ -63,23 +63,23 @@ public class Application {
 				".................................",
 				".................................");
 		
-		moon = new GameMap(groundFactory, moonMap);
-		world.addMap(moon);
+		moonMap = new GameMap(groundFactory, moonMapString);
+		world.addMap(moonMap);
 		
 		Actor player = new Player("Player", '@', 1, 100);
-		world.addPlayer(player, gameMap, 4, 19);
+		world.addPlayer(player, lairMap, 4, 19);
 		
 		Grunt testGoon = new Goon("Mongo", player);
-		gameMap.addActor(testGoon, 0, 0);
+		lairMap.addActor(testGoon, 0, 0);
 
 		Ninja testNinja = new Ninja("Norbert", player);
-		gameMap.addActor(testNinja, 14, 12);
+		lairMap.addActor(testNinja, 14, 12);
 
 		Grunt testGrunt = new Grunt("Obediah", player);
-		gameMap.addActor(testGrunt, 10, 5);
+		lairMap.addActor(testGrunt, 10, 5);
 		
 		Actor testQ = new Qnpc(player);
-		gameMap.addActor(testQ, 27, 4);
+		lairMap.addActor(testQ, 27, 4);
 		
 		Item body = new Item("Rocket Body", 'h');
 		Item engine = new Item("Rocket Engine", 'e');
@@ -88,14 +88,14 @@ public class Application {
 		testGoon.addItemToInventory(Item.newInventoryItem("Key", '$'));
 		testGrunt.addItemToInventory(Item.newInventoryItem("Key", '$'));
 		
-		gameMap.addItem(plan, 6, 3);
-		gameMap.addItem(new WeaponItem("Wristwatch Lazer", '>', 20, "burns"), 8, 3);
+		lairMap.addItem(plan, 6, 3);
+		lairMap.addItem(new WeaponItem("Wristwatch Lazer", '>', 20, "burns"), 8, 3);
 		
 		Miniboss miniboss = new Miniboss("Dr Maybe", player);
 		miniboss.addItemToInventory(Item.newInventoryItem("Rocket Engine", 'e'));
-		gameMap.addActor(miniboss, 26, 12);
+		lairMap.addActor(miniboss, 26, 12);
 
-		gameMap.addItem(body, 2, 1);
+		lairMap.addItem(body, 2, 1);
 			
 		world.run();
 
