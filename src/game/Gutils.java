9c02c6784027fc14e2e2a135c70b743dbbd73e19
MolarFox <molar@molarfox.io>
2019-05-23 21:19:21 +1000

    Fix issues with Goons, no longer extends Grunt

--- src/game/Gutils.java
+++ src/game/Gutils.java
@@ -2,6 +2,15 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * Static utility methods that can be used by any package classes
+ * All static classes, gutils = Game Utilities
+ * 
+ * @author      Rithesh Jayaram <RJAY0006@student.monash.edu> 
+ * @author      Sarah Dennis <SDEN0009@student.monash.edu>
+ * @version     1.0
+ * @since       1.0
+ */
 class Gutils {
 
     /**
